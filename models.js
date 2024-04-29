87e7fdf9520a7d760a526517ef40070227007556
raschkatie <raschkatie@gmail.com>
2024-04-29 15:05:58 -0400

    Adding validation methods

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+    bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
     Title: { type: String, required: true },
@@ -17,6 +18,16 @@ let userSchema = mongoose.Schema({
     Birthday: Date,
     FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
+
+// hashing password
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+};
+
+// no arrow functions when defining instance methods
+userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+};
 
 let directorSchema = mongoose.Schema({
     Name: { type: String, required: true },
