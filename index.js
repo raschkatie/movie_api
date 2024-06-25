b2674e11d9cafbd6d2c11ecf07bd1f1d0db98e87
raschkatie <raschkatie@gmail.com>
2024-06-25 17:02:11 -0400

    Removed CORS origin specification
    Threw a lot of errors that I don't know how to fix yet; may add back in

--- index.js
+++ index.js
@@ -12,18 +12,7 @@ const Users = Models.User;
 const app = express();
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflix-kr.netlify.app'];
-
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            let message = 'The CORS policy for this application doesn\'t allow acess from origin ' + origin;
-            return callback(new Error(message), false);
-        }
-        return callback(null, true);
-    }
-}));
+app.use(cors());
 
 const { check, validationResult } = require('express-validator');
 
