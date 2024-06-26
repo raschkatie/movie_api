b99c92346ce7ddbf842daa3a2da54f0f6b99dec0
raschkatie <raschkatie@gmail.com>
2024-06-26 16:51:10 -0400

    Temp removal of CORS issue

--- index.js
+++ index.js
@@ -12,17 +12,17 @@ const Users = Models.User;
 const app = express();
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflix-kr.netlify.app'];
+// let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflix-kr.netlify.app'];
 
 app.use(cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-        let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    }
+    // origin: (origin, callback) => {
+    //   if (!origin) return callback(null, true);
+    //   if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+    //     let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+    //     return callback(new Error(message), false);
+    //   }
+    //   return callback(null, true);
+    // }
   }));
 
 const { check, validationResult } = require('express-validator');
