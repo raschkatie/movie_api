1efbc9ee667851ebd73fe6c7fb4a9b86b81455ef
raschkatie <raschkatie@gmail.com>
2024-07-01 13:40:43 -0400

    Removed CORS specifications

--- index.js
+++ index.js
@@ -12,18 +12,7 @@ const Users = Models.User;
 const app = express();
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflix-kr.netlify.app', 'https://kr-my-flix.onrender.com'];
-
-app.use(cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin === -1)) { // If a specific origin isn’t found on the list of allowed origins
-        let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    }
-  }));
+app.use(cors());
 
 const { check, validationResult } = require('express-validator');
 
